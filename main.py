7516d924d3c5dcb38ac706aa3a71cb35fce6f097
Kazik <kaziksobo@outlook.com>
2022-11-30 23:22:18 +0000

    created test input and output

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+import flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
+# displays the home page
 @app.route('/')
-def main():
+def main() -> flask.Response:
     return render_template('index.html')
+
+@app.route('/message', methods=['POST', 'GET'])
+def test() -> flask.Response:
+    message = request.form['message']
+    return render_template('message.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
