088c7ee0272b08735455c405ee7e589aa164d759
Kazik <kaziksobo@outlook.com>
2022-12-01 10:20:21 +0000

    removed messages page (temp)

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def main() -> flask.Response:
     return render_template('index.html')
 
-@app.route('/message', methods=['POST', 'GET'])
-def test() -> flask.Response:
-    message = request.form['message']
-    return render_template('message.html', message=message)
+# @app.route('/message', methods=['POST', 'GET'])
+# def test() -> flask.Response:
+#     message = request.form['message']
+#     return render_template('message.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
