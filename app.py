075bbdbee5f08ffc908277b573553e7782962b81
Kazik <kaziksobo@outlook.com>
2023-01-19 16:40:16 +0000

    added message test - NOT RUN YET

--- app.py
+++ app.py
@@ -8,9 +8,17 @@ def main() -> flask.Response:
     """Displays the home page 'index.html'"""
     return render_template('index.html')
 
+@app.route('/message')
+def message() -> flask.Response:
+    user_message = request.form['message-input']
+    print(user_message)
+
+@app.route('/search')
+def search() -> flask.Response:
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
-
-
-
-
+    
+    
+    
