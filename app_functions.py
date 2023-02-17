56b9356dfc76cba1c9761e1b25117dff9ac8fdc2
Kazik <kaziksobo@outlook.com>
2023-02-17 13:19:02 +0000

    Added logging

--- app_functions.py
+++ app_functions.py
@@ -1,4 +1,6 @@
 from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+from csv import writer
+from os import stat
 
 def reply_generator(message: str) -> str:
     """Uses Blenderbot to generate a reply to the inputted message"""
@@ -6,8 +8,10 @@ def reply_generator(message: str) -> str:
 
     print(f'Input sequence: {message}')
 
+    # Declares tokenizer
     tokenizer = BlenderbotTokenizer.from_pretrained(name)
     
+    # Tokenizes input
     input_ids = tokenizer.encode(
         message,
         add_special_tokens=True,
@@ -17,7 +21,10 @@ def reply_generator(message: str) -> str:
     
     print(f'Input Ids: {input_ids}')
 
+    # Declares model
     model = BlenderbotForConditionalGeneration.from_pretrained(name)
+    
+    # Generates Reply ids
     reply_ids = model.generate(
         input_ids,
         num_beams=35,
@@ -25,4 +32,17 @@ def reply_generator(message: str) -> str:
     
     print(f'Reply Ids: {reply_ids}')
 
+    # Decodes reply ids
     return tokenizer.decode(reply_ids[0], skip_special_tokens=True)
+
+def log(user_message: str, bot_response: str, time_taken: float) -> None:
+    """Logs the user's message, the bot response and the computation time to a CSV file"""
+    file_address = 'log.csv'
+    with open(file_address, 'a', encoding='utf-8', newline='') as file_object:
+        csv_writer = writer(file_object)
+        # Checks if file did not already exist, and adds header if it did not
+        if stat(file_address).st_size == 0:
+            csv_writer.writerow(['User message', 'Bot response', 'Time taken'])
+        csv_writer.writerow([user_message, bot_response, time_taken])
+
+log('this is a second test message', 'This is second a test reply', 25)
