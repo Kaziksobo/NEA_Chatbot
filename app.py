a527531398c40535385e57522f28b3eff6fd0460
Kazik <kaziksobo@outlook.com>
2023-02-19 14:26:09 +0000

    Finished receiving input, generation and logging

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import flask
 from flask import Flask, render_template, request
-from app_functions import reply_generator
+from app_functions import reply_generator, log
 
 current_theme = 'light'
 current_page = 'index.html'
@@ -11,22 +11,33 @@ app = Flask(__name__)
 @app.route('/')
 def main() -> flask.Response:
     """Displays the home page 'index.html'"""
+    
     global stylesheet
     return render_template('index.html', stylesheet=stylesheet)
 
 @app.route('/message', methods=['GET', 'POST'])
 def message() -> flask.Response:
     """Requests the user's message, processes it and renders the messages page with the user's message and AI's reply"""
+    
     global stylesheet, current_page
+    
     user_message = request.form['message-input']
-    reply = reply_generator(user_message)
+    reply, time = reply_generator(user_message)
+    log(
+        user_message=user_message,
+        bot_response=reply,
+        time_taken=time
+    )
+    
     current_page = 'message.html'
     return render_template('message.html', stylesheet=stylesheet)
 
 @app.route('/theme', methods=['GET', 'POST'])
 def theme_switcher() -> flask.Response:
     """Switches the theme of the web app"""
+    
     global current_page, current_theme, stylesheet
+    
     if current_theme == 'light':
         current_theme = 'dark'
         stylesheet = 'static/dark-styles.css'
--- app_functions.py
+++ app_functions.py
@@ -1,13 +1,17 @@
 from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 from csv import writer, reader
 from os import stat, path, remove
+from time import time
+from typing import Union
 import torch
 
 def log_reader() -> list:
     """Reads the contents of the log file into a single list, ignoring the time taken column"""
+    
     # Checks if the chat history is empty
     if not path.exists('log.csv'):
         return None
+    
     with open('log.csv', 'r') as log_file:
         csv_reader = reader(log_file)
         chat_history = []
@@ -17,11 +21,14 @@ def log_reader() -> list:
                 continue
             # Appends the first two entries in the row to the chat_history list
             chat_history.extend((row[0], row[1]))
+    
     return chat_history
 
 
-def reply_generator(message: str) -> str:
+def reply_generator(message: str) -> Union[str, float]:
     """Uses Blenderbot to generate a reply to the inputted message"""
+    
+    start = time()
     name = 'facebook/blenderbot-400M-distill'
 
     print(f'Input sequence: {message}')
@@ -47,16 +54,25 @@ def reply_generator(message: str) -> str:
             is_split_into_words=False,
             return_tensors='pt'
         )
+        
+        print(f'Chat history Ids: {chat_history_ids}')
 
+        # Concatenating chat history and input tokens
         bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1)
 
         reply_ids = model_generation(name, bot_input_ids)
+        
         # Decodes reply ids
-        return tokenizer.decode(reply_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        reply = tokenizer.decode(reply_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
     else:
         reply_ids = model_generation(name, input_ids)
-        return tokenizer.decode(reply_ids[0], skip_special_tokens=True)
+        reply = tokenizer.decode(reply_ids[0], skip_special_tokens=True)
+
+    print(f'Response: {reply}')
+    end = time()
+    
+    return reply, round((end - start), 2)
 
 def model_generation(name: str, input_ids: torch.Tensor) -> torch.Tensor:
     """Generates response sequence using an already tokenized input"""
@@ -70,27 +86,32 @@ def model_generation(name: str, input_ids: torch.Tensor) -> torch.Tensor:
         max_length=60
     )
     print(f'Reply Ids: {result}')
+    
     return result
 
 def log(user_message: str, bot_response: str, time_taken: float) -> None:
     """Logs the user's message, the bot response and the computation time to a CSV file"""
+    
     file_address = 'log.csv'
     if not path.exists(file_address):
         create_log_file(file_address)
+    
     with open(file_address, 'r', encoding='utf-8', newline='') as file_object:
         delete_csv = sum(1 for _ in file_object) == 11
     if delete_csv:
+        print('Removing log file')
         remove(file_address)
         create_log_file(file_address)
     
+    print('Logging data')
     with open(file_address, 'a', encoding='utf-8', newline='') as file_object:
         csv_writer = writer(file_object)
         csv_writer.writerow([user_message, bot_response, time_taken])
 
 def create_log_file(file_address: str) -> None:
     """Creates the log file, adding in the header row"""
+    
+    print('Creating new log file')
     with open(file_address, 'w', encoding='utf-8', newline='') as file_object:
         csv_writer = writer(file_object)
         csv_writer.writerow(['User message', 'Bot response', 'Time taken'])
-
-log('This is a new test message', 'This is a new test response', 19.8)
