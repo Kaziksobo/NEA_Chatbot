bca6d726d390d0ff7d94e0020d9578e241f2bf94
Kazik <kaziksobo@outlook.com>
2023-03-01 15:27:36 +0000

    Added message search functionality

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import flask
 from flask import Flask, render_template, request
-from app_functions import reply_generator, log, log_reader
+from app_functions import reply_generator, log, log_reader, message_id_generator
 
 current_theme = 'light'
 current_page = 'index.html'
 stylesheet = 'static/light-styles.css'
 formatted_chat_history = []
+messages_to_display = []
 
 app = Flask(__name__)
 
@@ -30,16 +31,11 @@ def message() -> flask.Response:
         time_taken=time
     )
     
-    chat_history = log_reader()
-    formatted_chat_history = []
-    for i in range(min(9, len(chat_history))):
-        if i % 2 == 0:
-            formatted_chat_history.append({'type': 'ai', 'text': chat_history[i], 'id': f'message-{i + 1}'})
-        else:
-            formatted_chat_history.append({'type': 'user', 'text': chat_history[i], 'id': f'message-{i + 1}'})
+    chat_history = log_reader('log.csv', format=True, len_limit=True)
+    chat_history = message_id_generator(chat_history)
     
     current_page = 'message.html'
-    return render_template('message.html', stylesheet=stylesheet, messages=formatted_chat_history)
+    return render_template('message.html', stylesheet=stylesheet, messages=chat_history)
 
 @app.route('/theme', methods=['GET', 'POST'])
 def theme_switcher() -> flask.Response:
@@ -55,15 +51,29 @@ def theme_switcher() -> flask.Response:
         stylesheet = 'static/light-styles.css'
     if current_page == 'message.html': 
         return render_template(current_page, stylesheet=stylesheet, messages=formatted_chat_history)
+    elif current_page == 'search_result.html':
+        return render_template(current_page, stylesheet=stylesheet, messages_to_display=messages_to_display)
     else:
         return render_template(current_page, stylesheet=stylesheet)
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search() -> flask.Response:
-    pass
+    """Renders searched message plus number of surrounding messages"""
+    
+    global messages_to_display
+    
+    query = request.form['search-bar']
+
+    history = log_reader('ext_log.csv', format=True)
+    for message_info in history:
+        if message_info['text'] == query:
+            location = history.index(message_info)
+    
+    messages_to_display = [history[i] for i in range((location - 4), (location + 5))]
+    messages_to_display = message_id_generator(messages_to_display)
+    
+    current_page = 'search_result.html'
+    return render_template('search_result.html', stylesheet=stylesheet, messages=messages_to_display)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
-    
-    
-    
