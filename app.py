227c7a7ab66a6a87452be46b346d81ea7f05c2ab
Kazik <kaziksobo@outlook.com>
2023-03-09 12:25:22 +0000

    Added back button and full search functionality

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import flask
 from flask import Flask, render_template, request
-from app_functions import reply_generator, log, log_reader, message_id_generator
+from app_functions import reply_generator, log, log_reader, message_id_generator, format_message
 
 current_theme = 'light'
 current_page = 'index.html'
 stylesheet = 'static/light-styles.css'
-formatted_chat_history = []
+chat_history = []
 messages_to_display = []
+query_message = ''
 
 app = Flask(__name__)
 
@@ -21,9 +22,10 @@ def main() -> flask.Response:
 def message() -> flask.Response:
     """Requests the user's message, processes it and renders the messages page with the user's message and AI's reply"""
     
-    global stylesheet, current_page, formatted_chat_history
+    global stylesheet, current_page, chat_history
     
     user_message = request.form['message-input']
+    user_message = format_message(user_message)
     reply, time = reply_generator(user_message)
     log(
         user_message=user_message,
@@ -41,7 +43,7 @@ def message() -> flask.Response:
 def theme_switcher() -> flask.Response:
     """Switches the theme of the web app"""
     
-    global current_page, current_theme, stylesheet, formatted_chat_history
+    global current_page, current_theme, stylesheet, chat_history, query_message
     
     if current_theme == 'light':
         current_theme = 'dark'
@@ -49,10 +51,13 @@ def theme_switcher() -> flask.Response:
     else:
         current_theme = 'light'
         stylesheet = 'static/light-styles.css'
+    
     if current_page == 'message.html': 
-        return render_template(current_page, stylesheet=stylesheet, messages=formatted_chat_history)
+        return render_template(current_page, stylesheet=stylesheet, messages=chat_history)
     elif current_page == 'search_result.html':
-        return render_template(current_page, stylesheet=stylesheet, messages_to_display=messages_to_display)
+        return render_template(current_page, stylesheet=stylesheet, messages=messages_to_display)
+    elif current_page == 'search_error.html':
+        return render_template(current_page, stylesheet=stylesheet, message=query_message)
     else:
         return render_template(current_page, stylesheet=stylesheet)
 
@@ -60,20 +65,54 @@ def theme_switcher() -> flask.Response:
 def search() -> flask.Response:
     """Renders searched message plus number of surrounding messages"""
     
-    global messages_to_display
+    global messages_to_display, current_page, stylesheet, query_message
     
-    query = request.form['search-bar']
+    query_message = request.form['search-bar']
+    
+    print(f'Search query - {query_message}')
+
+    location = False
 
     history = log_reader('ext_log.csv', format=True)
+    
+    history.reverse()
+    
+    print(history)
+    
     for message_info in history:
-        if message_info['text'] == query:
+        if message_info['text'] == query_message:
+            print(f'Found message - {message_info}')
             location = history.index(message_info)
     
-    messages_to_display = [history[i] for i in range((location - 4), (location + 5))]
+    if not location:
+        current_page = 'search_error.html'
+        return render_template(current_page, stylesheet=stylesheet, message=query_message)
+    
+    messages_before = 0
+    messages_after = 0
+    messages_after_query = len(history) - (location + 1)
+    if messages_after_query > 3:
+        messages_after = 4
+        messages_before = 4
+    else:
+        messages_after = messages_after_query
+        messages_before = 8 - messages_after
+    
+    messages_to_display = [history[i] for i in range((location + (messages_after)), (location - (messages_before + 1)), -1)]
+    print(f'messages_to_display - {messages_to_display}')
     messages_to_display = message_id_generator(messages_to_display)
     
     current_page = 'search_result.html'
-    return render_template('search_result.html', stylesheet=stylesheet, messages=messages_to_display)
+    return render_template(current_page, stylesheet=stylesheet, messages=messages_to_display)
+
+@app.route('/back', methods=['GET', 'POST'])
+def back() -> flask.Response:
+    """Renders message page when back button on search results page clicked"""
+    
+    global chat_history, current_page, stylesheet
+    
+    current_page = 'message.html'
+    return render_template(current_page, stylesheet=stylesheet, messages=chat_history)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
